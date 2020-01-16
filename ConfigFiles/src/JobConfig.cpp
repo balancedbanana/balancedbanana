76c4dea7bc3c7d06e78ae8a3e2887e7c75322929
Niklas Lorenz <niklaslorenz1999@gmail.com>
2020-01-16 22:36:13 +0100

    Added method stubs in the configfile package

--- ConfigFiles/src/JobConfig.cpp
+++ ConfigFiles/src/JobConfig.cpp
@@ -5,3 +5,115 @@
 #include <JobConfig.h>
 
 using namespace balancedbanana::configfiles;
+
+JobConfig::JobConfig() {
+    //TODO implement
+}
+
+JobConfig::JobConfig(const std::stringstream &data) {
+    //TODO implement
+}
+
+JobConfig::JobConfig(const std::filesystem::path &path) {
+    //TODO implement
+}
+
+void JobConfig::set_min_ram(const std::optional <uint32_t> &miB) {
+    //TODO implement
+}
+
+void JobConfig::set_max_ram(const std::optional <uint32_t> &miB) {
+    //TODO implement
+}
+
+void JobConfig::set_min_cpu_count(const std::optional <uint32_t> &count) {
+    //TODO implement
+}
+
+void JobConfig::set_max_cpu_count(const std::optional <uint32_t> &count) {
+    //TODO implement
+}
+
+void JobConfig::set_blocking_mode(const std::optional<bool> &block_client) {
+    //TODO implement
+}
+
+void JobConfig::set_email(const std::string &email) {
+    //TODO implement
+}
+
+void JobConfig::set_priority(const std::optional <Priority> &priority) {
+    //TODO implement
+}
+
+void JobConfig::set_image(const std::string &path) {
+    //TODO implement
+}
+
+void JobConfig::set_environment(const std::optional <std::vector<std::string>> &environment) {
+    //TODO implement
+}
+
+void JobConfig::set_interruptible(const std::optional<bool> &interruptible) {
+    //TODO implement
+}
+
+void JobConfig::set_current_working_dir(const std::optional <std::filesystem::path> &cwd) {
+    //TODO implement
+}
+
+std::optional <uint32_t> JobConfig::min_ram() {
+    //TODO implement
+}
+
+std::optional <uint32_t> JobConfig::max_ram() {
+    //TODO implement
+}
+
+std::optional <uint32_t> JobConfig::min_cpu_count() {
+    //TODO implement
+}
+
+std::optional <uint32_t> JobConfig::max_cpu_count() {
+    //TODO implement
+}
+
+std::optional<bool> JobConfig::blocking_mode() {
+    //TODO implement
+}
+
+std::string &JobConfig::email() {
+    //TODO implement
+}
+
+std::optional <Priority> JobConfig::priority() {
+    //TODO implement
+}
+
+std::string &JobConfig::image() {
+    //TODO implement
+}
+
+std::optional <std::vector<std::string>> &JobConfig::environment() {
+    //TODO implement
+}
+
+std::optional<bool> JobConfig::interruptible() {
+    //TODO implement
+}
+
+std::optional <std::filesystem::path> &JobConfig::current_working_dir() {
+    //TODO implement
+}
+
+bool JobConfig::Save(const std::filesystem::path &path) {
+    //TODO implement
+}
+
+virtual void JobConfig::Serialize(const std::stringstream &destination) {
+    //TODO implement
+}
+
+void JobConfig::Merge(const JobConfig &config) {
+    //TODO implement
+}
--- ConfigFiles/src/SchedulerConfig.cpp
+++ ConfigFiles/src/SchedulerConfig.cpp
@@ -5,3 +5,35 @@
 #include <SchedulerConfig.h>
 
 using namespace balancedbanana::configfiles;
+
+SchedulerConfig::SchedulerConfig() {
+    //TODO implement
+}
+
+SchedulerConfig::SchedulerConfig(const std::filesystem::path &path) {
+    //TODO implement
+}
+
+size_t SchedulerConfig::count() {
+    //TODO implement
+}
+
+void SchedulerConfig::Set(const std::string &key, const std::string &value) {
+    //TODO implement
+}
+
+std::string &SchedulerConfig::Get(const std::string &key) {
+    //TODO implement
+}
+
+void SchedulerConfig::Clear() {
+    //TODO implement
+}
+
+bool SchedulerConfig::Save(const std::filesystem::path &path) {
+    //TODO implement
+}
+
+bool SchedulerConfig::Contains(const std::string &key) {
+    //TODO implement
+}
