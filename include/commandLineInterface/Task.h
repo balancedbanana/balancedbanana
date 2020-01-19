66851be54ac34d982d1adc08aa88fb195917201b
Thomas Häuselmann <thomashaeuselmann@web.de>
2020-01-19 15:07:07 +0100

    Roughly implement Command Line Parsing
    Update Task and JobConfig to be able to hold all command line arguments
    
    task is now much larger
    
    add cli11 standalone header to project

--- include/commandLineInterface/Task.h
+++ include/commandLineInterface/Task.h
@@ -1,25 +1,54 @@
 #pragma once
 
-#include <configfiles/JobConfig.h>
+#include <../configfiles/JobConfig.h>
 
-namespace balancedbanana {
-	namespace commandLineInterface {
+namespace balancedbanana
+{
+namespace commandLineInterface
+{
 
-		class Task {
-			std::string taskCommand;
-			std::shared_ptr<configfiles::JobConfig> config;
-			int type;
+class Task
+{
+	std::string taskCommand;
+	std::shared_ptr<configfiles::JobConfig> config;
+	int type;
+	std::string addImageName;
+	std::string addImageFilePath;
+	std::string removeImageName;
+	std::string serverIP;
+	std::string webAPIIP;
+	short int serverPort;
+	short int webAPIPort;
 
-		public:
+public:
+	void setType(int type);
+	int getType() const;
 
-			void setType(int type);
-			int getType() const;
+	void setTaskCommand(std::string &taskCommand);
+	const std::string &getTaskCommand() const;
 
-			void setTaskCommand(std::string& taskCommand);
-			const std::string& getTaskCommand() const;
+	std::shared_ptr<configfiles::JobConfig> getConfig() const;
 
-			std::shared_ptr<configfiles::JobConfig> getConfig() const;
+	void setAddImageName(std::string &addImageName);
+	const std::string &getAddImageName();
 
-		};
-	}
-}
+	void setAddImageFilePath(std::string &addImageFilePath);
+	const std::string &getAddImageFilePath();
+
+	void setRemoveImageName(std::string &removeImageName);
+	const std::string &getRemoveImageName();
+
+	void setServerIP(std::string &serverIP);
+	const std::string &getServerIP();
+
+	void setWebAPIIP(std::string &webAPIIP);
+	const std::string &getWebAPIIP();
+
+	void setServerPort(short int serverPort);
+	short int getServerPort();
+
+	void setWebAPIPort(short int webAPIPort);
+	short int getWebAPIPort();
+};
+} // namespace commandLineInterface
+} // namespace balancedbanana
