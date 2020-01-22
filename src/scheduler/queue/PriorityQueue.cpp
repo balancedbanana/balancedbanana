927f227c83be3f8483ec01670514bfefb1b61b86
Jonas <SirPiixel@gmail.com>
2020-01-22 21:11:56 +0100

    Added Queue and outline of PriorityQueue

---
+++ src/scheduler/queue/PriorityQueue.cpp
@@ -0,0 +1,26 @@
+#include "scheduler/queue/PriorityQueue.h"
+
+namespace balancedbanana {
+    namespace scheduler {
+
+        unsigned long PriorityQueue::getPos(unsigned long id) {
+            return 0;
+        }
+
+        Job PriorityQueue::getJob(uint32_t ram, uint32_t cores) {
+            return nullptr;
+        }
+
+        void PriorityQueue::update() {
+
+        }
+
+        PriorityQueue::PriorityQueue(Timer) {
+
+        }
+
+        void PriorityQueue::addTask(const Job &job) {
+
+        }
+    }
+}
