447717d7ca72e3a49ef496bea6a7b8b56a19d969
Thomas Häuselmann <thomashaeuselmann@web.de>
2020-01-26 15:06:15 +0100

    Changed misleading signature of CLP process

--- src/client/Client.cpp
+++ src/client/Client.cpp
@@ -6,6 +6,6 @@ using namespace balancedbanana::commandLineInterface;
 int main(int argc, char** argv) {
     CommandLineProcessor proc;
     // Wieso argc als Zeiger...
-    auto task = proc.process(&argc, argv);
+    auto task = proc.process(argc, argv);
     std::cout << "Task Type" << task->getType() << "\n";
 }
