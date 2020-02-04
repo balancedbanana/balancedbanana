3b52824da7ed63e36140d39a3a27fb06c7877672
Rakan Zeid Al Masri <rakanalmasri97@gmail.com>
2020-02-04 22:35:12 +0100

    Implemented JobGateway
    Changes:
    
    * Implemented JobGateway src and header
    * Added target for it in database CMakeLists

--- include/database/JobGateway.h
+++ include/database/JobGateway.h
@@ -1,12 +1,21 @@
 #pragma once
 
 #include "IGateway.h"
+#include "job_details.h"
+#include "job_result.h"
 
-namespace balancedbanana {
-    namespace database {
-        class JobGateway: virtual public IGateway {
-        public:
+namespace balancedbanana::database {
+    class JobGateway : virtual public IGateway {
+    public:
+        uint64_t add(details details) override;
+        bool remove(uint64_t id) override;
+        job_details getJob(uint64_t id);
+        std::vector<job_details> getJobs();
+        bool startJob(uint64_t job_id, uint64_t worker_id, Specs specs);
+        bool finishJob(uint64_t job_id, const QDateTime& finish_tim, const std::string& stdout, const int8_t exit_code);
+        job_result getJobResult(uint64_t job_id);
 
-        };
-    }
+    private:
+        static bool doesJobExist(uint64_t id);
+    };
 }
