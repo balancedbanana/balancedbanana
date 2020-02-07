2bafae50cec9dd0c143d121b454db566a591258b
niklaslorenz <niklaslorenz1999@gmail.com>
2020-02-07 13:56:02 +0100

    renamed the SchedulerConfig class to ApplicationConfig and adjusted headers, constructors, comments

--- include/configfiles/ApplicationConfig.h
+++ include/configfiles/ApplicationConfig.h
@@ -6,35 +6,35 @@
 namespace balancedbanana::configfiles {
 
 //This class contains policies for the scheduler as key-value pairs.
-    class SchedulerConfig {
+    class ApplicationConfig {
     private:
         //This attribute contains all the policies for the scheduler.
         std::map <std::string, std::string> values_;
 
 
     public:
-        //This constructor creates an empty SchedulerConfig.
-        SchedulerConfig();
+        //This constructor creates an empty ApplicationConfig.
+        ApplicationConfig();
 
-        //This constructor loads a SchedulerConfig from a file.
-        SchedulerConfig(const std::filesystem::path &path);
+        //This constructor loads a ApplicationConfig from a file.
+        ApplicationConfig(const std::filesystem::path &path);
 
-        //This method returns the amount of key-value pairs in the SchedulerConfig.
+        //This method returns the amount of key-value pairs in the ApplicationConfig.
         size_t count();
 
-        //This method adds policy with a specified key to the SchedulerConfig.
+        //This method adds policy with a specified key to the ApplicationConfig.
         void Set(const std::string &key, const std::string &value);
 
-        //This method returns the policy associated with the specified key. If the SchedulerConfig does not contain such a policy, an empty string is returned.
+        //This method returns the policy associated with the specified key. If the ApplicationConfig does not contain such a policy, an empty string is returned.
         std::string &Get(const std::string &key);
 
-        //This method clears out the SchedulerConfig.
+        //This method clears out the ApplicationConfig.
         void Clear();
 
-        //This method serializes the SchedulerConfig and saves it to the specified file.
+        //This method serializes the ApplicationConfig and saves it to the specified file.
         bool Save(const std::filesystem::path &path);
 
-        //This method returns true when the SchedulerConfig contains a policy associated with the specified key, otherwise it returns false.
+        //This method returns true when the ApplicationConfig contains a policy associated with the specified key, otherwise it returns false.
         bool Contains(const std::string &key);
 
     };
--- src/configfiles/ApplicationConfig.cpp
+++ src/configfiles/ApplicationConfig.cpp
@@ -2,41 +2,41 @@
 // Created by Niklas on 16.01.2020.
 //
 
-#include <configfiles/SchedulerConfig.h>
+#include <configfiles/ApplicationConfig.h>
 
 using namespace balancedbanana::configfiles;
 
-SchedulerConfig::SchedulerConfig() :
+ApplicationConfig::ApplicationConfig() :
     values_(){
 }
 
-SchedulerConfig::SchedulerConfig(const std::filesystem::path &path) {
+ApplicationConfig::ApplicationConfig(const std::filesystem::path &path) {
     //TODO implement
 }
 
-size_t SchedulerConfig::count() {
+size_t ApplicationConfig::count() {
     return values_.size();
 }
 
-void SchedulerConfig::Set(const std::string &key, const std::string &value) {
+void ApplicationConfig::Set(const std::string &key, const std::string &value) {
     if(!key.empty()) {
         values_[key] = value;
     }
 }
 
-std::string &SchedulerConfig::Get(const std::string &key) {
+std::string &ApplicationConfig::Get(const std::string &key) {
     return values_[key];
 }
 
-void SchedulerConfig::Clear() {
+void ApplicationConfig::Clear() {
     values_.clear();
 }
 
-bool SchedulerConfig::Save(const std::filesystem::path &path) {
+bool ApplicationConfig::Save(const std::filesystem::path &path) {
     //TODO implement
     return false;
 }
 
-bool SchedulerConfig::Contains(const std::string &key) {
+bool ApplicationConfig::Contains(const std::string &key) {
     return values_.find(key) != values_.end();
 }
