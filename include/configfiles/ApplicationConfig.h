e053e6aacc38449f2bf6901b479e55d6cdafbaec
niklaslorenz <niklaslorenz1999@gmail.com>
2020-02-07 14:13:31 +0100

    ApplicationConfig now inherits from map instead of wrapping it, added some constructors for ApplicationConfig

--- include/configfiles/ApplicationConfig.h
+++ include/configfiles/ApplicationConfig.h
@@ -5,37 +5,23 @@
 
 namespace balancedbanana::configfiles {
 
-//This class contains policies for the scheduler as key-value pairs.
-    class ApplicationConfig {
-    private:
-        //This attribute contains all the policies for the scheduler.
-        std::map <std::string, std::string> values_;
-
-
+//This class contains policies for the application as key-value pairs.
+    class ApplicationConfig : public std::map<std::string, std::string> {
     public:
         //This constructor creates an empty ApplicationConfig.
         ApplicationConfig();
 
         //This constructor loads a ApplicationConfig from a file.
-        ApplicationConfig(const std::filesystem::path &path);
+        explicit ApplicationConfig(const std::filesystem::path &path);
 
-        //This method returns the amount of key-value pairs in the ApplicationConfig.
-        size_t count();
+        explicit ApplicationConfig(std::istream &data);
 
-        //This method adds policy with a specified key to the ApplicationConfig.
-        void Set(const std::string &key, const std::string &value);
+        ApplicationConfig(const ApplicationConfig &);
 
-        //This method returns the policy associated with the specified key. If the ApplicationConfig does not contain such a policy, an empty string is returned.
-        std::string &Get(const std::string &key);
-
-        //This method clears out the ApplicationConfig.
-        void Clear();
+        void Serialize(std::ostream &data) const;
 
         //This method serializes the ApplicationConfig and saves it to the specified file.
-        bool Save(const std::filesystem::path &path);
-
-        //This method returns true when the ApplicationConfig contains a policy associated with the specified key, otherwise it returns false.
-        bool Contains(const std::string &key);
+        bool Save(const std::filesystem::path &path) const;
 
     };
 
--- src/configfiles/ApplicationConfig.cpp
+++ src/configfiles/ApplicationConfig.cpp
@@ -6,37 +6,23 @@
 
 using namespace balancedbanana::configfiles;
 
-ApplicationConfig::ApplicationConfig() :
-    values_(){
-}
+ApplicationConfig::ApplicationConfig() = default;
 
 ApplicationConfig::ApplicationConfig(const std::filesystem::path &path) {
     //TODO implement
 }
 
-size_t ApplicationConfig::count() {
-    return values_.size();
+ApplicationConfig::ApplicationConfig(std::istream &data) {
+    //TODO implement
 }
 
-void ApplicationConfig::Set(const std::string &key, const std::string &value) {
-    if(!key.empty()) {
-        values_[key] = value;
-    }
+ApplicationConfig::ApplicationConfig(const balancedbanana::configfiles::ApplicationConfig &original) = default;
+
+void ApplicationConfig::Serialize(std::ostream &data) const{
+
 }
 
-std::string &ApplicationConfig::Get(const std::string &key) {
-    return values_[key];
-}
-
-void ApplicationConfig::Clear() {
-    values_.clear();
-}
-
-bool ApplicationConfig::Save(const std::filesystem::path &path) {
+bool ApplicationConfig::Save(const std::filesystem::path &path) const{
     //TODO implement
     return false;
-}
-
-bool ApplicationConfig::Contains(const std::string &key) {
-    return values_.find(key) != values_.end();
 }
