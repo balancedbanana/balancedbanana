84d450e6d065ba560024216714e07f4bf3401eb1
Christopher Homberger <christopher.homberger@web.de>
2020-02-11 15:31:37 +0100

    Squashed errorhandling

--- src/worker/Docker.cpp
+++ src/worker/Docker.cpp
@@ -38,11 +38,8 @@ Container Docker::Start(int userid, const Task & task) {
     proc.setArguments(args);
     proc.start();
     proc.waitForFinished(-1);
-    if(proc.exitStatus() != QProcess::NormalExit) {
-        throw std::runtime_error("Invalid Argument for docker start");
-    }
     std::string output = proc.readAllStandardOutput().toStdString();
-    if(proc.exitCode() != 0) {
+    if(proc.exitStatus() != QProcess::NormalExit || proc.exitCode() != 0) {
         std::string err = proc.readAllStandardError().toStdString();
         throw std::runtime_error("Invalid Argument for docker start:\n" + output + "Error:\n" + err);
     }
