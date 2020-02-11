7aab93b17cbc1665a0da0ea0b27d043bba0190d9
Thomas <thomashaeuselmann@web.de>
2020-02-11 11:31:39 +0100

    Test Timer class
    turns out that pointers to vectors as member attributes is weird.
    Do not do that.

--- src/timedevents/Timer.cpp
+++ src/timedevents/Timer.cpp
@@ -19,10 +19,10 @@ void Timer::setInterval(unsigned int seconds)
 }
 
 
-void Timer::addTimerFunction(std::function<void()>* function)
+void Timer::addTimerFunction(std::function<void()>& function)
 {
     if (!this->active) {
-        this->timerFunctions->push_back(*function);
+        this->timerFunctions.push_back(function);
     }
 }
 
@@ -37,7 +37,7 @@ void Timer::start()
                 std::this_thread::sleep_for(std::chrono::seconds(this->delay));
 
                 if (this->active) {
-                    for (auto i = this->timerFunctions->begin(); i != this->timerFunctions->end(); ++i) {
+                    for (auto i = this->timerFunctions.begin(); i != this->timerFunctions.end(); ++i) {
                         (*i)();
                     }
                 }
