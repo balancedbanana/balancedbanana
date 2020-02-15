c728993aede19d56897402351ba9e2769bfbab1d
Christopher Homberger <christopher.homberger@web.de>
2020-02-15 17:57:21 +0100

    Added ssh AuthTest

--- testsrc/communication/authenticator/AuthTest.cpp
+++ testsrc/communication/authenticator/AuthTest.cpp
@@ -1,5 +1,6 @@
 #include "gtest/gtest.h"
 #include <communication/authenticator/Authenticator.h>
+#include <communication/authenticator/AuthHandler.h>
 
 TEST(authenticator, InvalidArguments)
 {
@@ -18,4 +19,15 @@ TEST(authenticator, GenerateSignature)
     auto p = balancedbanana::communication::authenticator::Authenticator::GeneratePrivatePublicKeyPair();
     auto s = balancedbanana::communication::authenticator::Authenticator::GenerateSignature("Hallo Welt", p.first);
     ASSERT_NE(s, "");
+}
+
+TEST(authenticator, AuthHandler)
+{
+    auto handler = balancedbanana::communication::authenticator::AuthHandler::GetDefault();
+    auto p = balancedbanana::communication::authenticator::Authenticator::GeneratePrivatePublicKeyPair();
+    auto sig = balancedbanana::communication::authenticator::Authenticator::GenerateSignature("steve", p.first);
+    ASSERT_NO_THROW(handler->authenticate(std::make_shared<balancedbanana::scheduler::IUser>("steve", p.second), "thePassword"));
+    ASSERT_ANY_THROW(handler->authenticate(std::make_shared<balancedbanana::scheduler::IUser>("steve2", p.second), "thePassword"));
+    ASSERT_ANY_THROW(handler->publickeyauthenticate(std::make_shared<balancedbanana::scheduler::IUser>("steve", p.second), sig));
+    ASSERT_ANY_THROW(handler->publickeyauthenticate(std::make_shared<balancedbanana::scheduler::IUser>("steve2", p.second), sig));
 }
