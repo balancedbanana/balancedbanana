7cc24a40b7efb7036b2c1cd95ec4cff79297192f
Thomas <thomashaeuselmann@web.de>
2020-02-18 13:22:17 +0100

    Add more test cases for Timer
    test behaviour of multiple timers running concurrently
    test behaviour of timer running out of scope (proper cleanup)

--- testsrc/timedevents/TestTimer.cpp
+++ testsrc/timedevents/TestTimer.cpp
@@ -48,7 +48,7 @@ TEST(Timer, LongInterval)
     static bool synchronizationFlag;
     synchronizationFlag = false;
 
-    timer.setInterval(30);
+    timer.setInterval(25);
     timer.addTimerFunction([&]() { synchronizationFlag = true; });
 
     EXPECT_FALSE(synchronizationFlag);
@@ -62,13 +62,64 @@ TEST(Timer, LongInterval)
     EXPECT_FALSE(synchronizationFlag) << "Timer Function should not have run after 15 seconds" << std::endl;
     std::this_thread::sleep_for(std::chrono::seconds(5));
     EXPECT_FALSE(synchronizationFlag) << "Timer Function should not have run after 20 seconds" << std::endl;
-    std::this_thread::sleep_for(std::chrono::seconds(5));
-    EXPECT_FALSE(synchronizationFlag) << "Timer Function should not have run after 25 seconds" << std::endl;
 
     std::this_thread::sleep_for(std::chrono::seconds(6));
-    EXPECT_TRUE(synchronizationFlag) << "Timer Function should have run after 31 seconds" << std::endl;
+    EXPECT_TRUE(synchronizationFlag) << "Timer Function should have run after 26 seconds" << std::endl;
     synchronizationFlag = false;
 
     timer.stop();
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
     EXPECT_FALSE(synchronizationFlag) << "After stopping, the timer should not run any of its timer functions again" << std::endl;
+}
+
+
+TEST(Timer, MultipleTimers)
+{
+    Timer timer1, timer2;
+
+    static bool sync1, sync2;
+    sync1 = false;
+    sync2 = false;
+
+    timer1.setInterval(1);
+    timer2.setInterval(1);
+
+    timer1.addTimerFunction([&]() { sync1 = true; });
+    timer2.addTimerFunction([&]() { sync2 = true; });
+
+    timer1.start();
+    timer2.start();
+
+    EXPECT_FALSE(sync1) << "Timer 1 should not have run yet" << std::endl;
+    EXPECT_FALSE(sync2) << "Timer 2 should not have run yet" << std::endl;
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(1100));
+
+    EXPECT_TRUE(sync1) << "Timer 1 should have run by now" << std::endl;
+    EXPECT_TRUE(sync2) << "Timer 2 should have run by now" << std::endl;
+
+    timer1.stop();
+
+    sync1 = false;
+    sync2 = false;
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(1100));
+
+    EXPECT_FALSE(sync1) << "Timer 1 should no longer run" << std::endl;
+    EXPECT_TRUE(sync2) << "Timer 2 should still be running" << std::endl;
+
+    timer2.stop();
+}
+
+TEST(Timer, OutOfScope)
+{
+    {
+        Timer timer;
+        timer.setInterval(1);
+        timer.addTimerFunction([&]() {});
+
+        timer.start();
+    }
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+    // Nothing should crash
 }
