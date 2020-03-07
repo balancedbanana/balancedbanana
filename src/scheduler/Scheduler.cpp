3ddd4c453fdfb30cb8f02d404a56b49dfd2afafe
Christopher Homberger <christopher.homberger@web.de>
2020-03-07 23:34:01 +0100

    Introduce the Scheduler class

--- src/scheduler/Scheduler.cpp
+++ src/scheduler/Scheduler.cpp
@@ -4,25 +4,60 @@
 #include <scheduler/httpserver/HttpServer.h>
 #include <iostream>
 #include <scheduler/Worker.h>
+#include <scheduler/Scheduler.h>
 
 using namespace balancedbanana::commandLineInterface;
 using namespace balancedbanana::communication;
 using namespace balancedbanana::scheduler;
+using balancedbanana::communication::Task;
+using balancedbanana::communication::TaskMessage;
+using balancedbanana::communication::Communicator;
+using balancedbanana::communication::TaskType;
+using balancedbanana::configfiles::ApplicationConfig;
+
+#ifdef _WIN32
+#define HOME_ENV "USERPROFILE"
+#else
+#define HOME_ENV "HOME"
+#endif
+
+Scheduler::Scheduler()
+{
+    task = std::make_shared<Task>();
+    auto configdir = std::filesystem::canonical(getenv(HOME_ENV)) / ".bbs";
+    std::filesystem::create_directories(configdir);
+    config = ApplicationConfig(configdir / "appconfig.ini");
+}
+
+void Scheduler::processCommandLineArguments(int argc, const char* const * argv)
+{
+    SchedulerCommandLineProcessor clp;
+    clp.process(argc, argv, task);
+    if(task->getType()) {
+        std::string server = "localhost";
+        short port = 8443;
+        if(!task->getServerIP().empty()) {
+            server = task->getServerIP();
+        } else if(config.Contains("server")) {
+            server = config["server"];
+        }
+        if(task->getServerPort()) {
+            port = task->getServerPort();
+        } else if(config.Contains("port")) {
+            port = std::stoi(config["port"]);
+        }
+
+        switch ((TaskType)task->getType())
+        {
+        default:
+            throw std::runtime_error("Sadly not implemented yet :(");
+            break;
+        }
+    }
+}
 
 int main(int argc, char** argv) {
-    SchedulerCommandLineProcessor proc;
-    std::shared_ptr<Task> task = std::make_shared<Task>();
-    if(proc.process(argc, argv, task)) {
-        return -1;
-    }
-    std::cout << "Task Type" << task->getType() << "\n";
-    // CommunicatorListener listener([](){ return std::make_shared<balancedbanana::communication::SchedulerClientMP>(); });
-    // std::vector<Worker> worker;
-    // listener.listen(task->getServerPort(), [&worker](std::shared_ptr<balancedbanana::communication::Communicator> com) {
-    //     worker.emplace_back()
-    // });
-    
-    // HttpServer server;
-    // server.listen(task->getWebAPIIP(), task->getWebAPIPort());
+    Scheduler scheduler;
+    scheduler.processCommandLineArguments(argc, argv);
     return 0;
 }
