2beba5f1218d290fa3990a16eddc212cb29fe632
Christopher Homberger <christopher.homberger@web.de>
2020-03-13 23:12:11 +0100

    Impl deadlock queue

--- src/scheduler/Scheduler.cpp
+++ src/scheduler/Scheduler.cpp
@@ -7,6 +7,8 @@
 #include <scheduler/Worker.h>
 #include <scheduler/Scheduler.h>
 #include <database/Repository.h>
+#include <scheduler/queue/PriorityQueue.h>
+#include <communication/message/TaskMessage.h>
 
 using namespace balancedbanana::commandLineInterface;
 using namespace balancedbanana::communication;
@@ -81,17 +83,86 @@ void Scheduler::processCommandLineArguments(int argc, const char* const * argv)
         }
 
         auto repo = std::make_shared<balancedbanana::database::Repository>(databasehost, databaseschema, databaseuser, databasepassword, databaseport);
+        auto queue = std::make_shared<balancedbanana::scheduler::PriorityQueue>(std::make_shared<timedevents::Timer>(), 360, 960);
+
+        // Reload scheduled Jobs from the database into the Queue
+        for(auto && job : repo->GetUnfinishedJobs()) {
+            if(job->getStatus() == balancedbanana::database::scheduled) {
+                queue->addTask(job);
+            }
+        }
+
+        struct QueueObserver : Observer<JobObservableEvent>, Observer<WorkerObservableEvent> {
+            std::shared_ptr<balancedbanana::scheduler::PriorityQueue> queue;
+            std::shared_ptr<balancedbanana::database::Repository> repo;
+
+            void OnUpdate(Observable<JobObservableEvent> *obsable, JobObservableEvent event) override {
+                auto job = (Job*)obsable;
+                if(job->getStatus() != balancedbanana::database::scheduled) {
+                    // Remove aborted, canced or other jobs from the queue
+                    queue->pullJob(job->getId());
+                }
+                if(job->getStatus() == balancedbanana::database::finished) {
+                    job->UnregisterObserver(this);
+                    for(auto && worker : repo->GetActiveWorkers()) {
+                        processWorkerload(worker.get());
+                    }
+                }
+            }
+
+            void OnUpdate(Observable<WorkerObservableEvent> *obsable, WorkerObservableEvent event) override {
+                if(event == WorkerObservableEvent::HARDWARE_DETAIL_UPDATE || event == WorkerObservableEvent::STATUS_UPDATE) {
+                    processWorkerload((Worker*)obsable);
+                }
+            }
+
+            void processWorkerload(Worker * worker) {
+                if(worker->getSpec().has_value() && worker->isConnected()) {
+                    auto spec = *worker->getSpec();
+                    for(auto && job : repo->GetUnfinishedJobs()) {
+                        if(job->getWorker_id() == worker->getId() && job->getStatus() == balancedbanana::database::processing) {
+                            // Reduce spec with running Jobs
+                            spec.cores -= job->getAllocated_cores();
+                            spec.ram -= job->getAllocated_ram();
+                        }
+                    }
+                    if(spec.ram > 4 /*4 MB limit avoid errors*/ && spec.cores > 0 /* 0 means no cores available for jobs*/ ) {
+                        if(auto job = queue->getJob(spec.ram, spec.cores)) {
+                            Task task;
+                            task.setConfig(job->getConfig());
+                            task.setTaskCommand(job->getCommand());
+                            task.setUserId(job->getUser()->id());
+                            job->setWorker_id(worker->getId());
+                            task.setType((uint32_t)TaskType::RUN);
+                            job->setAllocated_ram(std::min(job->getConfig()->max_ram().value_or((uint64_t)-1), spec.ram));
+                            job->setAllocated_cores(std::min(job->getConfig()->max_cpu_count().value_or((uint32_t)-1), spec.cores));
+                            job->setAllocated_osIdentifier(spec.osIdentifier);
+                            TaskMessage message(task);
+                            worker->send(message);
+                        }
+                    }
+                }
+            }
+        };
+
+        std::shared_ptr<QueueObserver> observer = std::make_shared<QueueObserver>();
+        observer->queue = queue;
+        observer->repo = repo;
 
         switch ((TaskType)task->getType())
         {
         case TaskType::SERVERSTART: {
-            clientlistener = std::make_shared<CommunicatorListener>([repo](){
+            clientlistener = std::make_shared<CommunicatorListener>([repo, observer](){
                 return std::make_shared<SchedulerClientMP>([repo](uint64_t id) -> std::shared_ptr<balancedbanana::scheduler::Job> {
                     return repo->GetJob(id);
                 }, [repo](uint64_t id, balancedbanana::database::JobStatus newstatus) -> void {
                     repo->GetJob(id)->setStatus(newstatus);
-                }, [repo](uint64_t userid, const std::shared_ptr<JobConfig>& config, const std::string& command) -> uint64_t {
-                    return repo->AddJob(userid, *config, QDateTime::currentDateTime(), command)->getId();
+                }, [repo, observer](uint64_t userid, const std::shared_ptr<JobConfig>& config, const std::string& command) -> uint64_t {
+                    auto job = repo->AddJob(userid, *config, QDateTime::currentDateTime(), command);
+                    // Add newly created Jobs to the Queue
+                    observer->queue->addTask(job);
+                    job->RegisterObserver(observer.get());
+                    return job->getId();
                 }, [repo](size_t uid, const std::string& username, const std::string& pubkey) -> std::shared_ptr<User> {
                     //Important for the worker to run Jobs under the right userid!!
                     return repo->AddUser(uid, username, "bot@localhost", pubkey);
@@ -106,13 +177,19 @@ void Scheduler::processCommandLineArguments(int argc, const char* const * argv)
                 mp->setClient(com);
                 com->detach();
             });
-            workerlistener = std::make_shared<CommunicatorListener>([repo](){
-                return std::make_shared<SchedulerWorkerMP>([repo](const std::string& name, const std::string& pubkey) -> std::shared_ptr<Worker> {
+            workerlistener = std::make_shared<CommunicatorListener>([repo, observer](){
+                return std::make_shared<SchedulerWorkerMP>([repo, observer](const std::string& name, const std::string& pubkey) -> std::shared_ptr<Worker> {
                     // TODO Rakan: Ich weiss nicht was hier genau passiert, aber specs hat kein space member
                     // 
-                    return repo->AddWorker(name, pubkey, { "", 32 * 1024, 50 }, "Why store an address");
-                }, [repo](const std::string &worker) -> std::shared_ptr<balancedbanana::scheduler::Worker> {
-                    return repo->FindWorker(worker);
+                    auto worker = repo->AddWorker(name, pubkey, { "", 32 * 1024, 50 }, "Why store an address");
+                    worker->RegisterObserver(observer.get());
+                    return worker;
+                }, [repo, observer](const std::string &worker) -> std::shared_ptr<balancedbanana::scheduler::Worker> {
+                    if(auto _worker = repo->FindWorker(worker)) {
+                        _worker->RegisterObserver(observer.get());
+                        return _worker;
+                    }
+                    return nullptr;
                 }, [repo](int jobid) -> std::shared_ptr<Job> {
                     return repo->GetJob(jobid);
                 });
