1a5f2760d1501a0a30a369e8a18067920f588f4e
Christopher Homberger <christopher.homberger@web.de>
2020-03-22 12:25:45 +0100

    Restructure Worker Infrastructure Mangle jobid in containername Automate buildimage Implement Get Jobs started in last n hours Disable addimage and removeimage feature

--- include/worker/docker/Docker.h
+++ include/worker/docker/Docker.h
@@ -13,10 +13,11 @@ namespace balancedbanana {
 			Container Run(const communication::Task& task);
 			/**
 			 * Build an image from a Dockerfile
-			 * @param name of the  created image
+			 * @param name of the created image
+			 * @param time of the dockerfile
 			 * @param dockerfilecontent content of the dockerfile to create a image from
 			 **/
-			void BuildImage(const std::string& name, const std::string& dockerfilecontent);
+			void BuildImage(const std::string& name, const std::string& dockerfilecontent, int64_t time = 0, const std::shared_ptr<balancedbanana::configfiles::JobConfig>& config = nullptr);
 			
 			/**
 			 * Remove an image from a Docker
--- src/worker/Docker.cpp
+++ src/worker/Docker.cpp
@@ -1,17 +1,39 @@
 #include <worker/docker/Docker.h>
 #include <QProcess>
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+#include <locale>
 
 using namespace balancedbanana::worker;
 using namespace balancedbanana::communication;
 
 Container Docker::Run(const Task & task) {
+    auto&& dockerfile = task.getAddImageFileContent();
+    if(!dockerfile.empty() && task.getBackupId().has_value()) {
+    QProcess proc;
+    proc.setProgram("docker");
+        auto imageid = (int64_t)*task.getBackupId();
+        proc.setArguments({ "images", "--format", "{{.ID}}", "--filter", QString::fromStdString("label=bbfiletime=" + std::to_string(imageid)), QString::fromStdString(task.getConfig()->image()) });
+        proc.start();
+        proc.waitForFinished(-1);
+        std::string output = proc.readAllStandardOutput().toStdString();
+        std::string err = proc.readAllStandardError().toStdString();
+        if(proc.exitStatus() == QProcess::NormalExit && proc.exitCode() == 0 && !output.empty() && output != "\n") {
+            std::cout << "Docker Image already built with id " << output;
+        } else {
+            std::cout << "Building Image" << task.getConfig()->image() << "\n";
+            BuildImage(task.getConfig()->image(), dockerfile, imageid, task.getConfig());
+        }
+    }
     QProcess proc;
     proc.setProgram("docker");
     auto config = task.getConfig();
     if(!task.getUserId()) {
         throw std::runtime_error("Invalid Argument userid needs a value");
     }
-    QStringList args = { "run", "-d", "--user", QString::fromStdString(std::to_string(*task.getUserId())), "--network", "host"};
+    std::string name = "bbdjob" + std::to_string(*task.getJobId());
+    QStringList args = { "run", "-d", "--name", QString::fromStdString(name), "--user", QString::fromStdString(std::to_string(*task.getUserId())), "--network", "host"};
     if(config->environment()) {
         for(auto && env : *config->environment()) {
             args.append({"-e", QString::fromStdString(env)});
@@ -25,7 +47,7 @@ Container Docker::Run(const Task & task) {
     }
     // Minimum only relevant for scheduler, might to check it here too
     if(config->max_cpu_count()) {
-        args.append({"--cpus", QString::fromStdString(std::to_string(*config->max_cpu_count()))});
+        args.append({"--cpuset-cpus", QString::fromStdString(std::to_string(*config->max_cpu_count()))});
     }
     if(!config->image().empty()) {
         args.append(QString::fromStdString(config->image()));
@@ -43,13 +65,23 @@ Container Docker::Run(const Task & task) {
         std::string err = proc.readAllStandardError().toStdString();
         throw std::runtime_error("Invalid Argument for docker start:\n" + output + "Error:\n" + err);
     }
-    return Container(output.substr(0, output.length() - 1));
+    return Container(name);
 }
 
-void balancedbanana::worker::Docker::BuildImage(const std::string& name, const std::string& dockerfilecontent) {
+void balancedbanana::worker::Docker::BuildImage(const std::string& name, const std::string& dockerfilecontent, int64_t time, const std::shared_ptr<balancedbanana::configfiles::JobConfig>& config) {
     QProcess proc;
     proc.setProgram("docker");
-    proc.setArguments({ "build", "-t", QString::fromStdString(name), "-"});
+    QStringList args { "build", "--label", QString::fromStdString("bbfiletime=" + std::to_string(time)), "-t", QString::fromStdString(name), "-"};
+    if(config) {
+        if(config->max_ram()) {
+            args.append({"-m", QString::fromStdString(std::to_string(*config->max_ram())) + "MB"});
+        }
+        // Minimum only relevant for scheduler, might to check it here too
+        if(config->max_cpu_count()) {
+            args.append({"--cpuset-cpus", QString::fromStdString(std::to_string(*config->max_cpu_count()))});
+        }
+    }
+    proc.setArguments(args);
     proc.start();
     proc.write(dockerfilecontent.data(), dockerfilecontent.length());
     proc.closeWriteChannel();
