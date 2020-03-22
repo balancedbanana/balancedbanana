c955245d15c2dfd21285449d0eaf5a86869b5ef2
niklaslorenz <niklaslorenz1999@gmail.com>
2020-03-22 16:32:53 +0100

    Changed the IGateway to only contain a QSqlDatabase pointer to provide it to its sub classes. IGateway is no longer responsible for creating and holding database connections.

--- include/database/IGateway.h
+++ include/database/IGateway.h
@@ -1,30 +1,14 @@
 #pragma once
 
-#include <string>
-#include <cinttypes>
-#include <mutex>
-#include <QThreadStorage>
-#include <QCache>
+#include <memory>
 #include <QSqlDatabase>
 
 namespace balancedbanana{
     namespace database{
         class IGateway{
-            struct Connection{
-                std::string host_name;
-                std::string databasename;
-                std::string username;
-                std::string password;
-                uint64_t port;
-            };
-            static std::mutex sync;
-            static Connection connection;
-            static QThreadStorage<QSqlDatabase> caches;
-        public:
-            static QSqlDatabase AcquireDatabase();
-            virtual ~IGateway(){};
-            static void createDBConnection(const std::string& host_name, const std::string& databasename, const
-            std::string& username, const std::string& password,  uint64_t port);
+        protected:
+            std::shared_ptr<QSqlDatabase> db;
+            explicit IGateway(std::shared_ptr<QSqlDatabase> db);
         };
     }
 }
