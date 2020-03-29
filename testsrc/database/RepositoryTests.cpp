e03399a3e4dff00456db34acc4efaca14e51fa6b
niklaslorenz <niklaslorenz1999@gmail.com>
2020-03-29 17:12:36 +0200

    Added more tests for Repository

--- testsrc/database/RepositoryTests.cpp
+++ testsrc/database/RepositoryTests.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 #include <database/Utilities.h>
 #include <database/JobStatus.h>
+#include <communication/MessageProcessor.h>
 #include <QSqlQuery>
 #include <QDebug>
 
@@ -91,7 +92,7 @@ TEST_F(RepositoryTest, AddJob_GetJob) {
     EXPECT_EQ(ptr->getCommand(), "command");
 }
 
-TEST_F(RepositoryTest, AlterJob_ChangeStatus) {
+TEST_F(RepositoryTest, AlterJob) {
     auto user = repo->AddUser(1, "name", "email", "public key");
     std::shared_ptr<Job> job = nullptr;
     QDateTime time = QDateTime::currentDateTime();
@@ -105,7 +106,7 @@ TEST_F(RepositoryTest, AlterJob_ChangeStatus) {
     repo->WriteBack();
     repo->ClearCache();
     auto newJob = job;
-    EXPECT_NO_THROW(job = repo->GetJob(id));
+    EXPECT_NO_THROW(newJob = repo->GetJob(id));
     EXPECT_NE(newJob, nullptr);
     EXPECT_EQ(newJob->getStatus(), canceled);
     EXPECT_EQ(newJob->getFinished_at(), finishTime);
@@ -137,6 +138,42 @@ TEST_F(RepositoryTest, AddWorker_GetWorker) {
     EXPECT_EQ(ptr->getSpec()->osIdentifier, specs.osIdentifier);
 }
 
+TEST_F(RepositoryTest, AlterWorker) {
+    auto worker = repo->AddWorker("name", "public key", {"os", 2, 1});
+    uint64_t id = worker->getId();
+    worker->setSpec(std::optional<Specs>({"os", 3, 1}));
+    repo->WriteBack();
+    repo->ClearCache();
+    auto newWorker = worker;
+    EXPECT_NO_THROW(newWorker = repo->GetWorker(id));
+    EXPECT_NE(newWorker, nullptr);
+    EXPECT_EQ(newWorker->getSpec()->ram, 3);
+    EXPECT_NE(newWorker, worker);
+}
+
+TEST_F(RepositoryTest, FindWorker) {
+    auto worker1Id = repo->AddWorker("worker 1", "public key", {"os", 2, 1})->getId();
+    auto worker2Id = repo->AddWorker("worker 2", "public key", {"os", 2, 1})->getId();
+    repo->WriteBack();
+    repo->ClearCache();
+    EXPECT_EQ(repo->FindWorker("worker 1")->getId(), worker1Id);
+    EXPECT_EQ(repo->FindWorker("worker 2")->getId(), worker2Id);
+    auto worker3 = repo->AddWorker("worker 3", "public key", {"os", 2, 1});
+    EXPECT_EQ(repo->FindWorker("worker 3"), worker3);
+}
+
+TEST_F(RepositoryTest, GetWorkers) {
+    Specs specs{"os", 2, 1};
+    auto worker1 = repo->AddWorker("worker 1", "public key", specs);
+    auto worker2 = repo->AddWorker("worker 2", "public key", specs);
+    auto worker3 = repo->AddWorker("worker 3", "public key", specs);
+    repo->WriteBack();
+    repo->ClearCache();
+    EXPECT_EQ(repo->GetWorkers().size(), 3);
+    auto worker4 = repo->AddWorker("worker 4", "public key", specs);
+    EXPECT_EQ(repo->GetWorkers().size(), 4);
+}
+
 TEST_F(RepositoryTest, TimedWriteBack) {
     auto user = repo->AddUser(1, "name", "my super unique email", "public key");
     std::this_thread::sleep_for(std::chrono::seconds(3));
@@ -147,7 +184,7 @@ TEST_F(RepositoryTest, TimedWriteBack) {
     EXPECT_EQ(user->name(), "name");
 }
 
-TEST_F(RepositoryTest, FlushCache) {
+TEST_F(RepositoryTest, FlushCache_RemoveUser) {
     auto user = repo->AddUser(1, "name", "email", "public key");
     user->setEmail("new email");
     repo->FlushCache();
@@ -161,11 +198,27 @@ TEST_F(RepositoryTest, FlushCache) {
     EXPECT_NE((uint64_t) user.get(), usrptr);
 }
 
+TEST_F(RepositoryTest, FlushCache_RemoveJob) {
+    auto user = repo->AddUser(1, "name", "email", "public key");
+    auto job = repo->AddJob(1, JobConfig(), QDateTime::currentDateTime(), "command");
+    uint64_t id = job->getId();
+    job->setCommand("new command");
+    repo->FlushCache();
+    auto newJob = repo->GetJob(id);
+    EXPECT_EQ(job, newJob);
+    auto jobptr = (uint64_t) job.get();
+    job = nullptr;
+    newJob = nullptr;
+    repo->FlushCache();
+    job = repo->GetJob(id);
+    EXPECT_NE((uint64_t) job.get(), jobptr);
+}
+
 TEST_F(RepositoryTest, GetJobsInInterval_OldJob) {
     auto user = repo->AddUser(1, "name", "email", "public key");
     QDateTime beginTime = QDateTime::currentDateTime();
-    auto job1 = repo->AddJob(1, JobConfig(), beginTime.addSecs(1), "finished job command");
-    auto job2 = repo->AddJob(1, JobConfig(), beginTime.addSecs(2), "started job command");
+    auto job1 = repo->AddJob(1, JobConfig(), beginTime.addSecs(1), "job1 command");
+    auto job2 = repo->AddJob(1, JobConfig(), beginTime.addSecs(2), "job2 command");
     uint64_t id1 = job1->getId();
     job1->setStatus(scheduled);
     job2->setStatus(scheduled);
@@ -200,4 +253,24 @@ TEST_F(RepositoryTest, GetJobsInInterval_ChangedJob) {
     job->setScheduled_at(beginTime.addSecs(3));
     EXPECT_EQ(repo->GetJobsInInterval(beginTime.addSecs(2), beginTime.addSecs(4), scheduled).size(), 1);
     EXPECT_EQ(repo->GetJobsInInterval(beginTime, beginTime.addSecs(2), scheduled).size(), 0);
+}
+
+TEST_F(RepositoryTest, GetUnfinishedJobs) {
+    auto user = repo->AddUser(1, "name", "email", "public key");
+    QDateTime time = QDateTime::currentDateTime();
+    auto job1 = repo->AddJob(1, JobConfig(), time, "command");
+    auto job2 = repo->AddJob(1, JobConfig(), time, "command");
+    auto id = job1->getId();
+    job1->setStatus(processing);
+    job2->setStatus(finished);
+    repo->WriteBack();
+    repo->ClearCache();
+    EXPECT_EQ(repo->GetUnfinishedJobs().size(), 1);
+    EXPECT_EQ(repo->GetUnfinishedJobs()[0]->getId(), id);
+    auto job3 = repo->AddJob(1, JobConfig(), time, "command");
+    job3->setStatus(processing);
+    EXPECT_EQ(repo->GetUnfinishedJobs().size(), 2);
+    auto job4 = repo->AddJob(1, JobConfig(), time, "command");
+    job4->setStatus(canceled);
+    EXPECT_EQ(repo->GetUnfinishedJobs().size(), 2);
 }
